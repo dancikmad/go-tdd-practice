e49e4b2c0c5f3a8bfb7c6ca5ecde0963e9d4eb45
Daniel Crigan <crgdaniel@yahoo.com>
2025-03-04 20:52:05 +0000

    chore: refactor code

--- hello.go
+++ hello.go
@@ -2,33 +2,40 @@ package main
 
 import "fmt"
 
-const spanish = "Spanish"
-const french = "French"
-const russian = "Russian"
+const (
+  spanish = "Spanish"
+  french = "French"
+  russian = "Russian"
 
-const englishHelloPrefix = "Hello, "
-const spanishHelloPrefix = "Hola, "
-const frenchHelloPrefix = "Bonjour, "
-const russianHelloPrefix = "Привет, "
+  englishHelloPrefix = "Hello, "
+  spanishHelloPrefix = "Hola, "
+  frenchHelloPrefix = "Bonjour, "
+  russianHelloPrefix = "Привет, "
+)
+
 
 
 func Hello(name string, language string) string {
   if name == "" {
       name = "World"
   }
-  prefix := englishHelloPrefix
-
-  switch language {
-  case spanish:
-      prefix = spanishHelloPrefix
-  case french:
-      prefix = frenchHelloPrefix
-  case russian:
-      prefix = russianHelloPrefix
-  }
-
-  return prefix + name 
+  return greetingPrefix(language) + name
 }
+
+func greetingPrefix(language string) (prefix string) {
+    switch language {
+    case french:
+        prefix = frenchHelloPrefix
+    case spanish:
+        prefix = spanishHelloPrefix
+    case russian:
+        prefix = russianHelloPrefix
+    default:
+        prefix = englishHelloPrefix
+    }
+    return prefix
+}
+
 
 func main() {
   fmt.Println(Hello("world", ""))
